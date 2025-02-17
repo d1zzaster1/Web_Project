093baf23876a7c304d5eacc2c192028749a15554
R.Yerassyl <145887770+d1zzaster1@users.noreply.github.com>
2025-02-17 20:26:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ function changeLanguage(language) {
         document.getElementById('punctual').textContent = 'Punctual';
         document.getElementById('disciplined').textContent = 'Disciplined';
         document.getElementById('ambivert').textContent = 'Ambivert';
-    } else if(language === 'kz'){
-        document.getElementById('title').textContent = 'Профиль Рахатов Ерасыл 10\'С';
-        document.getElementById('description').textContent = 'Сәлем, менің атым Ерасыл, мен 16 жастамын және менің профильме қош келіңіз.';
-        document.getElementById('name').textContent = 'Аты';
-        document.getElementById('age').textContent = 'Жасы';
-        document.getElementById('email').textContent = 'Электронды почта';
+    } else if(language === 'ru'){
+        document.getElementById('title').textContent = 'Профиль Рахатова Ерасыла 10\'С';
+        document.getElementById('description').textContent = 'Приве, меня зовут Рахатов Ерасыл, мне 16 лет и добро пожаловать на мой профиль.';
+        document.getElementById('name').textContent = 'Имя';
+        document.getElementById('age').textContent = 'Возраст';
+        document.getElementById('email').textContent = 'Электронная почта';
         document.getElementById('name-value').textContent = 'Ерасыл';
         document.getElementById('age-value').textContent = '16';
         document.getElementById('email-value').textContent = 'rahatoverasyl7@gmail.com';
-        document.getElementById('hardworking').textContent = 'Еңбекқор';
-        document.getElementById('punctual').textContent = 'Пунктуалды';
-        document.getElementById('disciplined').textContent = 'Тәртіпті';
+        document.getElementById('hardworking').textContent = 'Трудолюбивый';
+        document.getElementById('punctual').textContent = 'Пунктуальный';
+        document.getElementById('disciplined').textContent = 'Дисциплинированный';
         document.getElementById('ambivert').textContent = 'Амбиверт';
     }
 }
